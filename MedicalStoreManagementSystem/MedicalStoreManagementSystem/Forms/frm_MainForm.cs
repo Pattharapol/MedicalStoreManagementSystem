0a91582e1d0eaaaa78d2abeed982c25282af0bda
unknown <tjtik83@gmail.com>
2021-04-28 16:14:45 +0700

    'first'

---
+++ MedicalStoreManagementSystem/MedicalStoreManagementSystem/Forms/frm_MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace MedicalStoreManagementSystem.Forms
+{
+    public partial class frm_MainForm : DevExpress.XtraEditors.XtraForm
+    {
+        public frm_MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void categoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Category frm = new frm_Category();
+            frm.ShowDialog();
+        }
+    }
+}
